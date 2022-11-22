edb3c044289d12718029a1643c1fc2ebd1564595
Aleksandr Dukhovnyy <script@vip-person.net>
2022-11-23 00:00:22 +0200

    added CD

---
+++ scripts/cd.sh
@@ -0,0 +1,32 @@
+#!/bin/bash;
+
+date=$(date +"%Y.%m.%d-%H:%M");
+tag=$(git describe --tags);
+build_name="build_$tag.$date";
+
+cd ../;
+
+# npm run build;
+
+mkdir CD;
+cd CD;
+
+git clone https://github.com/Aleksandr-JS-Developer/suncase.git gh-pages;
+git checkout gh-pages;
+
+cd gh-pages;
+ls | xargs rm -rf;
+rm .gitignore;
+
+cd ../..;
+
+cp -r dist/. CD/gh-pages/;
+
+$SHELL;
+
+# git add .;
+# git commit -m "$build_name";
+# git push;
+
+# cd ../..;
+# ls;
